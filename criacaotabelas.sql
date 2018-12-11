ac619ec0a2a0701d701a712f094bc76b92b4ce8b
Pedro Coelho <rabbit11@users.noreply.github.com>
2018-12-11 09:16:02 -0200

    Add files via upload

---
+++ criacaotabelas.sql
@@ -0,0 +1,32 @@
+﻿create table ARTISTA(
+	Nome varchar(50) primary key,
+	PaisDeOrigem varchar(50) NOT NULL
+);
+
+create table ALBUM(
+	TituloAlbum varchar(50) PRIMARY KEY,
+	NomeArtista varchar(50) NOT NULL,
+	OrdemCronologica smallint, --utilizar trigger (campo calculado)
+	Genero varchar(20) NOT NULL,
+	AnoDeLancamento SMALLINT NOT NULL,
+	foreign key (NomeArtista) references Artista(Nome)
+		on delete cascade
+);
+
+create table PLAYLIST(
+	NomePl varchar(50) primary key,
+	Autor varchar(50) NOT NULL
+);
+
+create table MUSICA(
+	TituloMusica varChar(50),
+	TituloAlbum varchar(50),
+	NomePl varchar(50) NOT NULL,
+	Duracao smallint NOT NULL,
+	Faixa smallint NOT NULL,
+	primary key (TituloAlbum, TituloMusica),
+	foreign key (TituloAlbum) references Album(TituloAlbum)
+		on delete cascade,
+	foreign key (NomePl) references Playlist(NomePl)
+		on delete cascade
+);
---
+++ insercaotuplas.sql
@@ -0,0 +1,21 @@
+INSERT INTO ARTISTA VALUES('Kurt Cobain', 'Inglaterra');
+INSERT INTO ARTISTA VALUES('Drake', 'Alemanha');
+INSERT INTO ARTISTA VALUES('Post Malone', 'Australia');
+INSERT INTO ARTISTA VALUES ('Foster the People', 'Brasil');
+
+INSERT INTO ALBUM VALUES('Torches', 'Foster the People', null, 'Rock', '2001');
+INSERT INTO ALBUM VALUES('Hamptons', 'Drake', null, 'Rap', '2018');
+INSERT INTO ALBUM VALUES('Magic', 'Kurt Cobain', null, 'Hard Rock', '2012');
+INSERT INTO ALBUM VALUES('Scorpion', 'Post Malone', null, 'Rap', '2017');
+
+INSERT INTO PLAYLIST VALUES('Mixtape #1', 'Crepschi');
+INSERT INTO PLAYLIST VALUES('Classicos de Natal', 'Crepschi Natalino');
+INSERT INTO PLAYLIST VALUES('Mixtape de Ferias', 'Natham de Ferias');
+INSERT INTO PLAYLIST VALUES('Hottest Mixtape', 'Pedro');
+
+INSERT INTO MUSICA VALUES('Pumped up Kicks', 'Torches', 'Mixtape de Ferias', '3', '1');
+INSERT INTO MUSICA VALUES('I Got a Feeling', 'Hamptons', 'Classicos de Natal', '4', '1');
+INSERT INTO MUSICA VALUES('In My Feelings', 'Magic', 'Classicos de Natal', '2', '2');
+INSERT INTO MUSICA VALUES('Gods Plan', 'Scorpion', 'Mixtape #1', '17', '1');
+INSERT INTO MUSICA VALUES('Sandras Rose', 'Scorpion', 'Hottest Mixtape', '5', '2');
+
