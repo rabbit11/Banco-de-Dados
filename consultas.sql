53884a9ac78c61d38604acadb22b20242d39c5fb
Pedro Coelho <rabbit11@users.noreply.github.com>
2019-01-14 17:36:37 -0200

    Add files via upload

--- consultas.sql
+++ consultas.sql
@@ -1,20 +1,29 @@
 -- Consulta 1
 SELECT * FROM ALBUM WHERE genero = <genero>;
 
+-- Consulta 2
+
+
 -- Consulta 3
 SELECT * FROM MUSICA WHERE tituloalbum = <album>;
 
 -- Consulta 4
-SELECT nomeartista FROM ALBUM WHERE titulomuisca = <musica>;
+SELECT nomeartista FROM ALBUM WHERE titulomusica = <musica>;
 
 -- Consulta 5
-SELECT faixa FROM MUSICA WHERE titulomuisca = <musica>;
+SELECT faixa FROM MUSICA WHERE titulomusica = <musica>;
 
 -- Consulta 6
 SELECT * FROM ALBUM WHERE anodelancamento = <ano>;
 
 -- Consulta 7
 SELECT * FROM ALBUM WHERE nomeartista = <nome>;
+
+-- Consulta 8
+SELECT DISTINCT titulomusica FROM MUSICA, ALBUM, ARTISTA WHERE MUSICA.tituloalbum = ALBUM.tituloalbum AND ALBUM.nomeartista = <nome>;
+
+-- Consulta 9
+SELECT * FROM ALBUM WHERE tituloalbum = <album>;
 
 -- Consulta 10
 SELECT COUNT(*) FROM ALBUM;
@@ -31,5 +40,8 @@ SELECT * FROM MUSICA;
 -- Consulta 14
 SELECT * FROM MUSICA WHERE nomepl = <playlist>;
 
---consulta extra, conta numero de albuns de determinado artista, utilizando a view
+-- Consulta 15
+SELECT PLAYLIST.nomepl AS Playlist, COUNT(*) AS Numero_Musicas FROM MUSICA, PLAYLIST WHERE MUSICA.nomepl = PLAYLIST.nomepl GROUP BY PLAYLIST.nomepl;
+
+-- Consulta 16, conta numero de albuns de determinado artista, utilizando a view
 SELECT * from NUMERO_ALBUNS
